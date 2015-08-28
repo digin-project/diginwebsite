1cf7a3d75b79941e5f78737e55296ea17e4945de
bltnico <bellot.n@gmail.com>
2015-08-28 14:53:20 +0200

    Fix typo

--- billing/stripe_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("API_KEY", "pk_test_2FLv7aQdFn9kd9c7Ca3WRIqi");
-define("API_SECRET", "sk_test_XT1iGrNPaIYsYgfEYgHrBGmT");
-define("FORM_SECRET", "732a06cb5b6776c385a89c24d3b1ab81319af09c9a412ab9");
-?>
